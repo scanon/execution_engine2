ddf837f054522d3c74db4336f3cd41bd753834fc
Boris Sadkhin <boris@bortech.us>
2019-07-02 15:03:52 -0500

    Added skeleton

--- lib/execution_engine2/MethodRunner.py
+++ lib/execution_engine2/MethodRunner.py
@@ -1,8 +1,9 @@
 import json
 import os
-import time
+from time import time
 from configparser import ConfigParser
 from typing import Dict
+
 
 
 class MethodRunner:
@@ -18,14 +19,11 @@ class MethodRunner:
     @staticmethod
     def _get_config() -> ConfigParser:
         parser = ConfigParser()
-        parser.read(os.environ.get("KB_DEPLOYMENT_CONFIG"))
+        # parser.read(os.environ.get("KB_DEPLOYMENT_CONFIG"))
         return parser
 
     def __init__(self, ctx):
         self.parser = self._get_config()
-        self.njs_db_name = self.parser.get("NarrativeJobService", "mongodb-database")
-        self.njs_jobs_collection_name = "exec_tasks"
-        self.njs_logs_collection_name = "exec_logs"
         self.ctx = ctx
 
     def check_job_cancelled(self, params: Dict[str, str]):
@@ -34,10 +32,9 @@ class MethodRunner:
         }
 
     def status(self):
-        return json(
-            {
-                "servertime": f"{time.time()}",
-            })
+        return {
+                "servertime": f"{time()}",
+            }
 
     def check_job_permissions(self):
         pass
