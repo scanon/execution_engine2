5d1696830cb159aa03cc5fdcb17d701dae1b82fa
Boris Sadkhin <boris@bortech.us>
2019-07-08 23:03:30 -0500

    Added a job model

---
+++ lib/execution_engine2/models/create_job.py
@@ -0,0 +1,28 @@
+from mongoengine import connect
+from lib.execution_engine2.models.models import Job, JobOutput, JobInput
+
+connect('jobs')
+
+job = Job()
+output = JobOutput()
+input = JobInput()
+
+job.user = "test"
+job.authstrat = 'workspace'
+job.wsid = "1234"
+
+output.version = "1"
+output.id = "1234"
+output.result = {}
+
+input.wsid = job.wsid
+input.name = 'SpecialJob'
+input.service_ver = "Version123"
+input.method = "Method"
+input.app_id = "App_id"
+
+job.job_input = input
+job.job_output = output
+
+
+job.save()
