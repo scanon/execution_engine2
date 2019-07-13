0b743a1ff47b478b84eb9f9c8eeeb1a160a65960
Boris Sadkhin <boris@bortech.us>
2019-07-12 19:44:51 -0500

    Submitting jobs as KBASE

--- lib/execution_engine2/utils/Condor.py
+++ lib/execution_engine2/utils/Condor.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 import htcondor
 
+
 from execution_engine2.utils.Scheduler import Scheduler
 
 
@@ -11,25 +12,56 @@ class Condor(Scheduler):
     def cleanup_submit_file(self, submit_filepath):
         pass
 
+    def setup_environment_vars(self, params, config):
+        # 7 day docker job timeout default, Catalog token used to get access to volume mounts
+        environment_vars = {
+            "DOCKER_JOB_TIMEOUT": config.get("DOCKER_JOB_TIMEOUT", "604800"),
+            "KB_ADMIN_AUTH_TOKEN": config.get("KB_ADMIN_AUTH_TOKEN"),
+            "KB_AUTH_TOKEN": params.get("TOKEN"),
+            "CLIENTGROUP": params.get("CLIENTGROUP"),
+            "JOB_ID": params.get("JOB_ID"),
+            "WORKDIR": f"{config.get('WORKDIR')}/{params.get('USER')}/{params.get('JOB_ID')}",
+            "CONDOR_ID": "$(Cluster).$(Process)",
+        }
+
+        environment = ""
+        for key, val in environment_vars():
+            environment += f"{key}={val}"
+
+        return environment
+
     def submit_job(self, params, config):
-        
+        sub = htcondor.Submit({})
+        sub["executable"] = config["executable"]
+        sub["arguments"] = list(params.get("job_id"), config.get("endpoint"))
+        sub["environment"] = self.setup_environment_vars(params, config)
+        sub["universe"] = "vanilla"
+        sub["+AccountingGroup"] = params.get("USER")
+        sub["Concurrency_Limits"] = params.get("USER")
+        sub["OWNER"] = config.get("POOL_USER", "condor_pool")
+        sub["ShouldTransferFiles"] = config.get("SHOULD_TRANSFER_FILES", "YES")
+        sub["When_To_Transfer_Output"] = config.get(
+            "WHEN_TO_TRANSFER_OUTPUT", "ON_EXIT"
+        )
 
-
-
+        # with schedd.transaction() as txn:
+        #     print(sub.queue(txn, 2))
 
     def run_submit_file(self, submit_filepath):
         pass
 
     def get_job_info(self, batch_name=None, cluster_id=None):
         if batch_name is not None and cluster_id is not None:
-            return self.job_info(info={}, error=Exception(
-                "Please use only batch name or cluster_id, not both"))
+            return self.job_info(
+                info={},
+                error=Exception("Please use only batch name or cluster_id, not both"),
+            )
 
         constraint = None
         if batch_name:
-            constraint = f'JobBatchName=?={batch_name}'
+            constraint = f"JobBatchName=?={batch_name}"
         if cluster_id:
-            constraint = f'ClusterID=?={cluster_id}'
+            constraint = f"ClusterID=?={cluster_id}"
 
         try:
             job = htcondor.Schedd().query(constraint=constraint, limit=1)[0]
--- test/execution_engine2_scheduler_test.py
+++ test/execution_engine2_scheduler_test.py
@@ -3,6 +3,11 @@ import unittest
 
 import htcondor
 import classad
+import logging
+import pwd
+import os
+
+logging.basicConfig(level=logging.INFO)
 
 from execution_engine2.utils.Condor import Condor
 
@@ -13,6 +18,50 @@ class execution_engine2Test(unittest.TestCase):
     def setUpClass(cls):
         cls.condor = Condor()
 
+        cls.check_kbase_user()
+        cls.schedd = htcondor.Schedd()
+        if cls.queue_is_empty():
+            cls.add_a_sleep_job()
+        jobs = cls.queue_status()
+        for job in jobs:
+            logging.info(f"clusterid {job.get('ClusterId')} running cmd ( {job.get('Cmd')} ) is in state {job.get('JobStatus')}")
+
+
+
+
+
+    @classmethod
+    def add_a_sleep_job(cls):
+        logging.info("Adding a sleep job")
+        sub = htcondor.Submit({})
+        sub['executable'] = '/bin/sleep'
+        sub['arguments'] = '5m'
+        sub['user'] = 'kbase'
+        with cls.schedd.transaction() as txn:
+            print(sub.queue(txn, 1))
+
+    @classmethod
+    def check_kbase_user(cls):
+        my_uid = os.getuid()
+        kbase_uid = pwd.getpwnam("kbase").pw_uid
+        if my_uid != kbase_uid:
+            logging.error(f"I'm not the KBASE User. My UID is {my_uid}. The KBASE uid is {kbase_uid}")
+            raise Exception
+        logging.info("Success. I'm the KBASE User")
+
+
+    @classmethod
+    def queue_is_empty(cls):
+        if len( cls.schedd.query(limit=1)) == 0:
+            logging.info("Queue is empty")
+            return True
+        return False
+
+    @classmethod
+    def queue_status(cls):
+        return cls.schedd.query()
+
+
     @classmethod
     def tearDownClass(cls):
         if hasattr(cls, 'wsName'):
@@ -20,7 +69,7 @@ class execution_engine2Test(unittest.TestCase):
             print('Test workspace was deleted')
 
     def test_create_scheduler(self):
-        print("Ok")
+
         scheduler = self.condor
         schedd = htcondor.Schedd()
         #
@@ -32,5 +81,3 @@ class execution_engine2Test(unittest.TestCase):
 
         job_id = 1
          # type: classad.ClassAd
-
-        print(job.printJson())
