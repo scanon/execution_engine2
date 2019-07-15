dee8180a56006106b13b2957a573fe25509cd3e1
Boris Sadkhin <boris@bortech.us>
2019-07-15 00:44:24 -0500

    Test submit file

--- lib/execution_engine2/utils/Condor.py
+++ lib/execution_engine2/utils/Condor.py
@@ -1,13 +1,83 @@
+import json
 from collections import namedtuple
+from configparser import ConfigParser
 
 import htcondor
-
 
 from execution_engine2.utils.Scheduler import Scheduler
 
 
 class Condor(Scheduler):
     job_info = namedtuple("job_info", "info error")
+    submission_info = namedtuple("submission_info", "clusterid submit error")
+    job_resource = namedtuple("job_resource", "amount unit")
+
+    # TODO: Should these be outside of the class?
+    REQUEST_CPUS = "request_cpus"
+    REQUEST_MEMORY = "request_memory"
+    REQUEST_DISK = "request_disk"
+    CG = "+CLIENTGROUP"
+    EE2 = "execution_engine2"
+    ENDPOINT = 'kbase-endpoint'
+    EXECUTABLE = 'executable'
+
+    DEFAULT_CLIENT_GROUP = "default_client_group"
+
+    def __init__(self, config_filepath):
+        self.config = ConfigParser()
+        self.config.read(config_filepath)
+        self.ee_endpoint = self.config.get(section=self.EE2, option=self.ENDPOINT)
+        self.executable = self.config.get(section=self.EE2, option=self.EXECUTABLE)
+
+    def get_default_client_group_and_requirements(self, client_group):
+        """
+        Search the config file for default client groups and requirements
+        :param client_group: The section of the config file to search for requirements
+        :return: The default requirements for that client group, or for no client group provided
+        """
+        default_resources = dict()
+        if client_group in self.config.sections():
+            section = client_group
+            default_resources[self.CG] = client_group
+        else:
+            section = self.config['DEFAULT'][self.DEFAULT_CLIENT_GROUP]
+            default_resources[self.CG] = section
+
+        for item in [self.REQUEST_CPUS, self.REQUEST_DISK, self.REQUEST_MEMORY]:
+            default_resources[item] = self.config.get(section=section, option=item)
+
+        return default_resources
+
+    def get_client_group_and_requirements(self, cgr, json_input=False):
+        """
+          Example CGR string = njs,required_cpus=1,required_mem=5
+        :param cgr:
+        :param json_input:
+        :return:
+        """
+
+        reqs = dict()
+        client_group = None
+        if json_input is False:
+            cgr_split = cgr.split(",")  # List
+            client_group = cgr_split.pop(0)
+            requirements = {self.CG: client_group}
+            for item in cgr_split:
+                (req, value) = item.split("=")
+                requirements[req] = value
+            reqs = requirements
+        else:
+            reqs = json.loads(cgr)
+
+        default_requirements = self.get_default_client_group_and_requirements(
+            client_group
+        )
+        for key, value in default_requirements.items():
+            if key not in reqs or reqs[key].strip() is '':
+
+                reqs[key] = value
+
+        return reqs
 
     def cleanup_submit_file(self, submit_filepath):
         pass
@@ -25,27 +95,54 @@ class Condor(Scheduler):
         }
 
         environment = ""
-        for key, val in environment_vars():
-            environment += f"{key}={val}"
+        for key, val in environment_vars.items():
+            environment += f"{key}={val} "
 
         return environment
 
-    def submit_job(self, params, config):
+    def create_submit_file(self, params, config):
         sub = htcondor.Submit({})
-        sub["executable"] = config["executable"]
-        sub["arguments"] = list(params.get("job_id"), config.get("endpoint"))
+        sub["executable"] = self.executable
+        sub["arguments"] = " ".join([params.get("job_id"), self.ee_endpoint])
         sub["environment"] = self.setup_environment_vars(params, config)
         sub["universe"] = "vanilla"
-        sub["+AccountingGroup"] = params.get("USER")
-        sub["Concurrency_Limits"] = params.get("USER")
-        sub["OWNER"] = config.get("POOL_USER", "condor_pool")
-        sub["ShouldTransferFiles"] = config.get("SHOULD_TRANSFER_FILES", "YES")
+        print(params.get("user"))
+        sub["+AccountingGroup"] = params.get("user")
+        sub["Concurrency_Limits"] = params.get("user")
+        sub["+Owner"] = config.get("POOL_USER", "condor_pool")
+        sub["ShouldTransferFiles"] = self.config.get(
+            section=self.EE2, option="SHOULD_TRANSFER_FILES", fallback="YES"
+        )
         sub["When_To_Transfer_Output"] = config.get(
             "WHEN_TO_TRANSFER_OUTPUT", "ON_EXIT"
         )
 
-        # with schedd.transaction() as txn:
-        #     print(sub.queue(txn, 2))
+        cg_and_params = self.get_client_group_and_requirements(
+            cgr=params["client_group_and_requirements"]
+        )
+        print(cg_and_params)
+        for key, value in cg_and_params.items():
+            sub[key] = value
+
+        return sub
+
+    def run_job(self, params, config):
+        """
+        TODO: Add a retry
+        TODO: Add list of required params
+        :param params: Params to run the job, such as the username, job_id,
+        :param config: Configuration for the ee2 service, such as the njs endpoint, timeouts
+        :return:
+        """
+
+        sub = self.create_submit_file(params, config)
+        try:
+            with htcondor.Schedd.transaction() as txn:
+                return self.submission_info(
+                    clusterid=sub.queue(txn, 1), submit=sub, error=None
+                )
+        except Exception as e:
+            return self.submission_info(None, submit=sub, error=e)
 
     def run_submit_file(self, submit_filepath):
         pass
--- test/execution_engine2_scheduler_test.py
+++ test/execution_engine2_scheduler_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+
 import htcondor
 import classad
 import logging
@@ -13,10 +14,12 @@ from execution_engine2.utils.Condor import Condor
 
 
 class execution_engine2Test(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        cls.condor = Condor()
+        cls.deploy = "deploy.cfg"
+        cls.condor = Condor(cls.deploy)
+        cls.job_id = "1234"
+        cls.user = "kbase"
 
         cls.check_kbase_user()
         cls.schedd = htcondor.Schedd()
@@ -24,19 +27,17 @@ class execution_engine2Test(unittest.TestCase):
             cls.add_a_sleep_job()
         jobs = cls.queue_status()
         for job in jobs:
-            logging.info(f"clusterid {job.get('ClusterId')} running cmd ( {job.get('Cmd')} ) is in state {job.get('JobStatus')}")
-
-
-
-
+            logging.info(
+                f"clusterid {job.get('ClusterId')} running cmd ( {job.get('Cmd')} ) is in state {job.get('JobStatus')}"
+            )
 
     @classmethod
     def add_a_sleep_job(cls):
         logging.info("Adding a sleep job")
         sub = htcondor.Submit({})
-        sub['executable'] = '/bin/sleep'
-        sub['arguments'] = '5m'
-        sub['user'] = 'kbase'
+        sub["executable"] = "/bin/sleep"
+        sub["arguments"] = "5m"
+        sub["user"] = "kbase"
         with cls.schedd.transaction() as txn:
             print(sub.queue(txn, 1))
 
@@ -45,14 +46,15 @@ class execution_engine2Test(unittest.TestCase):
         my_uid = os.getuid()
         kbase_uid = pwd.getpwnam("kbase").pw_uid
         if my_uid != kbase_uid:
-            logging.error(f"I'm not the KBASE User. My UID is {my_uid}. The KBASE uid is {kbase_uid}")
+            logging.error(
+                f"I'm not the KBASE User. My UID is {my_uid}. The KBASE uid is {kbase_uid}"
+            )
             raise Exception
         logging.info("Success. I'm the KBASE User")
 
-
     @classmethod
     def queue_is_empty(cls):
-        if len( cls.schedd.query(limit=1)) == 0:
+        if len(cls.schedd.query(limit=1)) == 0:
             logging.info("Queue is empty")
             return True
         return False
@@ -61,23 +63,69 @@ class execution_engine2Test(unittest.TestCase):
     def queue_status(cls):
         return cls.schedd.query()
 
-
     @classmethod
     def tearDownClass(cls):
-        if hasattr(cls, 'wsName'):
-            cls.wsClient.delete_workspace({'workspace': cls.wsName})
-            print('Test workspace was deleted')
+        if hasattr(cls, "wsName"):
+            cls.wsClient.delete_workspace({"workspace": cls.wsName})
+            print("Test workspace was deleted")
 
-    def test_create_scheduler(self):
+    def _create_sample_params(self):
+        params = dict()
+        params["job_id"] = self.job_id
 
-        scheduler = self.condor
-        schedd = htcondor.Schedd()
-        #
-        # sub = htcondor.Submit({})
-        # sub['executable'] = '/bin/sleep'
-        # sub['arguments'] = '5m'
-        # with schedd.transaction() as txn:
-        #     print(sub.queue(txn, 2))
+    def test_create_submit_file(self):
+        c = Condor("deploy.cfg")
+        params = {
+            "job_id": "test_job_id",
+            "user": "test",
+            "client_group_and_requirements": "",
+        }
+        config = {
+            "executable": '"/kb/deployment/misc/sdklocalmethodrunner.sh"',
+            "kbase-endpoint": "{{ kbase_endpoint }}",
+        }
 
-        job_id = 1
-         # type: classad.ClassAd
+        default_sub = c.create_submit_file(params, config)
+
+        sub = default_sub
+        self.assertEqual(sub["executable"], config["executable"])
+        self.assertEqual(
+            sub["arguments"], f"{params['job_id']} {config['kbase-endpoint']}"
+        )
+        self.assertEqual(sub["universe"], "vanilla")
+        self.assertEqual(sub["+AccountingGroup"], params["user"])
+        self.assertEqual(sub["Concurrency_Limits"], params["user"])
+        self.assertEqual(sub["+Owner"], "condor_pool")
+        self.assertEqual(sub["ShouldTransferFiles"], "YES")
+        self.assertEqual(sub["When_To_Transfer_Output"], "ON_EXIT")
+        # TODO test config here or otherplace
+        self.assertEqual(sub["+CLIENTGROUP"], "njs")
+        self.assertEqual(sub[Condor.REQUEST_CPUS], c.config['njs'][Condor.REQUEST_CPUS])
+        self.assertEqual(sub[Condor.REQUEST_MEMORY], c.config['njs'][Condor.REQUEST_MEMORY])
+        self.assertEqual(sub[Condor.REQUEST_DISK], c.config['njs'][Condor.REQUEST_DISK])
+
+
+        params = {
+            "job_id": "test_job_id",
+            "user": "test",
+            "client_group_and_requirements": "njs,request_cpus=8,request_memory=10GB,request_apples=5",
+        }
+        config = {
+            "executable": '"/kb/deployment/misc/sdklocalmethodrunner.sh"',
+            "kbase-endpoint": "{{ kbase_endpoint }}",
+        }
+
+
+        njs_sub = c.create_submit_file(params, config)
+        sub = njs_sub
+
+        self.assertEqual(sub["+CLIENTGROUP"], "njs")
+        self.assertEqual(sub[Condor.REQUEST_CPUS], '8')
+        self.assertEqual(sub[Condor.REQUEST_MEMORY], '10GB')
+        self.assertEqual(sub[Condor.REQUEST_DISK], c.config['njs'][Condor.REQUEST_DISK])
+
+        print(sub)
+        (
+            "environment",
+            "DOCKER_JOB_TIMEOUT=604800 KB_ADMIN_AUTH_TOKEN=None KB_AUTH_TOKEN=None CLIENTGROUP=None JOB_ID=None WORKDIR=None/None/None CONDOR_ID=$(Cluster).$(Process) ",
+        )
