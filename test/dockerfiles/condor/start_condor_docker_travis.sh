97eefeabde8e79b5a42a7c9acc96ae3618e6f7aa
Boris Sadkhin <boris@bortech.us>
2019-07-18 16:55:05 -0500

    Condor Integration tests

--- test/dockerfiles/condor/start_condor_docker_travis.sh
+++ test/dockerfiles/condor/start_condor_docker_travis.sh
@@ -3,8 +3,8 @@
 # TODO Pull this variable from a config
 export docker_tag=kbase/ee2:condor_test_instance
 docker pull ${docker_tag}
-docker rm condor --force
-cnt_id=`docker run -i -d  --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /Users/:/Users/ --name condor  -p 9618:9618 ${docker_tag} /usr/sbin/init`
+docker rm condor_test_container --force
+cnt_id=`docker run -i -d  --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /Users/:/Users/ --name condor_test_container  -p 9618:9618 ${docker_tag} /usr/sbin/init`
 echo $cnt_id
 sleep 3
 docker exec -i -u 0 ${cnt_id} ./start_condor.sh
