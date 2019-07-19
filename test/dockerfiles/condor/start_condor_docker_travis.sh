f08dd5be75898d65952f3e43c7b241139ae00607
Boris Sadkhin <boris@bortech.us>
2019-07-19 15:26:29 -0500

    Fixing codacy

--- test/dockerfiles/condor/start_condor_docker_travis.sh
+++ test/dockerfiles/condor/start_condor_docker_travis.sh
@@ -4,7 +4,7 @@
 export docker_tag=kbase/ee2:condor_test_instance
 docker pull ${docker_tag}
 docker rm condor_test_container --force
-cnt_id=`docker run -i -d  --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro  --name condor_test_container  --network host ${docker_tag} /usr/sbin/init`
+cnt_id=$(docker run -i -d  --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro  --name condor_test_container  --network host ${docker_tag} /usr/sbin/init)
 echo "Container id is $cnt_id"
 docker ps
 docker exec -d -i -u 0 ${cnt_id} ./start_condor.sh
