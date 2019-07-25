e1387ed28767bf37f0fea677a709e89d6cc5a2c0
Boris Sadkhin <boris@bortech.us>
2019-07-25 17:49:45 -0500

    Attempting to add job runner

--- scripts/download_runner.sh
+++ scripts/download_runner.sh
@@ -6,8 +6,6 @@ runner_dir=/runner
 mkdir -p ${runner_dir} && cd ${runner_dir} && rm -rf JobRunner
 git clone https://github.com/kbase/JobRunner.git
 cd JobRunner
-sleep 5
 python3 -m venv venv && . ./venv/bin/activate
-sleep 5
 pip install -r requirements.txt
 tar -czvf ${runner_dir}/JobRunner.tgz $runner_dir/JobRunner
