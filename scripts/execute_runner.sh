84eb6b5bf467dbb3d5042838d45289ab3f6ad192
Boris Sadkhin <boris@bortech.us>
2019-08-23 11:26:48 -0500

    Fixing python exe

--- scripts/execute_runner.sh
+++ scripts/execute_runner.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -x
 
-
 #TODO Attempt to automatically find a compatible version of python with the correct libs
+#OTHERWISE, PYTHON BIN IS $PYTHON_EXECUTABLE
 
-export PATH=$PATH:$PYTHON_BIN_PATH
 export HOME=$(pwd)
-which python > which_python
+
 env > envf
-echo "export PYTHON_BIN_PATH=$PYTHON_BIN_PATH ">> env_file
+echo "export PYTHON_EXECUTABLE=$PYTHON_EXECUTABLE ">> env_file
 echo "export KB_ADMIN_AUTH_TOKEN=$KB_ADMIN_AUTH_TOKEN ">> env_file
 echo "export KB_AUTH_TOKEN=$KB_AUTH_TOKEN ">> env_file
 echo "export DOCKER_JOB_TIMEOUT=$DOCKER_JOB_TIMEOUT ">> env_file
@@ -17,9 +16,7 @@ echo "export JOB_ID=$JOB_ID ">> env_file
 echo "export DELETE_ABANDONED_CONTAINERS=$DELETE_ABANDONED_CONTAINERS ">> env_file
 
 
-python -V > pythonV
-
-/miniconda/bin/python -V > pyversion
+$PYTHON_EXECUTABLE -V > pyversion
 
 
 JOB_ID=$1
@@ -29,9 +26,9 @@ export KBASE_ENDPOINT
 
 tar -xvf JobRunner.tgz && cd JobRunner && cp scripts/jobrunner.py . && chmod +x jobrunner.py
 
-echo "/miniconda/bin/python ./jobrunner.py ${JOB_ID} ${EE2_ENDPOINT}" > cmd
+echo "$PYTHON_EXECUTABLE ./jobrunner.py ${JOB_ID} ${EE2_ENDPOINT}" > cmd
 
-/miniconda/bin/python ./jobrunner.py ${JOB_ID} ${EE2_ENDPOINT} > jobrunner.out 2> jobrunner.error &
+$PYTHON_EXECUTABLE ./jobrunner.py ${JOB_ID} ${EE2_ENDPOINT} > jobrunner.out 2> jobrunner.error &
 pid=$!
 #TODO TRAP IN PYTHON
 #TODO MAKE SURE CONDOR_RM STILL WORKS IF PYTHON FAILS
